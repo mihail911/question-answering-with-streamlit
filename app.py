0068acf4fc83df1a23f702915ce0e2c29c10c223
adimyth <mishraaditya6991@gmail.com>
2020-09-22 16:53:35 +0530

    fix(app): removes streamlit caching

--- app.py
+++ app.py
@@ -1,28 +1,22 @@
 from typing import Dict
 
-import streamlit as st
-import wikipedia
-from transformers import Pipeline
-from transformers import pipeline
+import streamlit as st  # type: ignore
+import wikipedia    # type: ignore
+from transformers import Pipeline, pipeline # type: ignore
 
 NUM_SENT = 10
 
 
-@st.cache
 def get_qa_pipeline() -> Pipeline:
-    qa = pipeline('question-answering')
+    qa = pipeline("question-answering", framework="tf")
     return qa
 
 
 def answer_question(pipeline: Pipeline, question: str, paragraph: str) -> Dict:
-    input = {
-        "question": question,
-        "context": paragraph
-    }
+    input = {"question": question, "context": paragraph}
     return pipeline(input)
 
 
-@st.cache
 def get_wiki_paragraph(query: str) -> str:
     results = wikipedia.search(query)
     try:
@@ -34,7 +28,13 @@ def get_wiki_paragraph(query: str) -> str:
 
 
 def format_text(paragraph: str, start_idx: int, end_idx: int) -> str:
-    return paragraph[:start_idx] + "**" + paragraph[start_idx:end_idx] + "**" + paragraph[end_idx:]
+    return (
+        paragraph[:start_idx]
+        + "**"
+        + paragraph[start_idx:end_idx]
+        + "**"
+        + paragraph[end_idx:]
+    )
 
 
 if __name__ == "__main__":
@@ -44,20 +44,19 @@ if __name__ == "__main__":
     paragraph_slot = st.empty()
     wiki_query = st.text_input("WIKIPEDIA SEARCH TERM", "")
     question = st.text_input("QUESTION", "")
-    
+
     if wiki_query:
         wiki_para = get_wiki_paragraph(wiki_query)
         paragraph_slot.markdown(wiki_para)
         # Execute question against paragraph
         if question != "":
             pipeline = get_qa_pipeline()
-            st.write(pipeline.model)
-            st.write(pipeline.model.config)
             try:
                 answer = answer_question(pipeline, question, wiki_para)
-                
                 start_idx = answer["start"]
                 end_idx = answer["end"]
-                paragraph_slot.markdown(format_text(wiki_para, start_idx, end_idx))
-            except:
-                st.write("You must provide a valid wikipedia paragraph")
+                st.success(answer["answer"])
+                print(f"QUESTION: {question}\nRESPONSE: {answer}")
+            except Exception as e:
+                print(e)
+                st.warning("You must provide a valid wikipedia paragraph")
