12443a906e88b09235d3a71929a5d640cd44b0ae
adimyth <mishraaditya6991@gmail.com>
2020-09-24 13:58:21 +0530

    build(requirements): changes to torch backend for heroku deployment

--- config.py
+++ config.py
@@ -2,5 +2,5 @@ from typing import Dict, Union
 
 from attrdict import AttrDict  # type: ignore
 
-config: Dict[str, Union[int, str]] = {"framework": "tf", "NUM_SENT": 10}
+config: Dict[str, Union[int, str]] = {"framework": "pt", "NUM_SENT": 10}
 config = AttrDict(config)
